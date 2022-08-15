2ad18d3cb33edc883cc0833c555824af55d727d6
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-15 14:52:16 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+   if(ride<=400){
+    return 'This one is on me!'
+  } else if(ride>2000 && ride<=2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if(ride>2500){
+    return 'No can do.'
+  } 
 }
+scuberGreetingForFeet(2400);
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city=="NYC"? "Ok, sounds good." : "No go."
+
+
 }
+console.log(ternaryCheckCity("NYC"));
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+    
+    case "not as generous":
+      return "Thank you."
+    
+    default:
+      return "Bye."
+    
+
+  }
+
 }
